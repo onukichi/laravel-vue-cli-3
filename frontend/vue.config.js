20c8b1894838917853678b1764fc483a3815a2fe
onukichi <onuonu007@gmail.com>
2020-08-07 21:29:19 +0900

    fix prod url

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // proxy API requests to Valet during development
   devServer: {
-    proxy: 'http://laracon.test'
+    proxy: 'https://laravel-vue-cli-3.herokuapp.com'
   },
 
   // output built static files to Laravel's public dir.
